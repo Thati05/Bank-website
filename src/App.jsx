a35aa45ceb5e009bb80fd81807715559e818c927
Seithati <151874357+Thati05@users.noreply.github.com>
2024-04-13 06:38:54 +0200

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 
-import Home from "./components/Home";
+import Home from "./Home";
 import Payments from "./components/Payments";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
-
 
 const App = () => {
   return (
     <>
       <Navbar />
+
       <div>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/" element={<Payments />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/payments" element={<Payments />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
-
       </div>
 
     </>
-
-
   )
 }
 
-export default App
+export default App;
