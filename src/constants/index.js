2162d774142b3d2d48208c7fe850cdc9059bf7ef
Seithati <151874357+Thati05@users.noreply.github.com>
2024-04-13 23:28:52 +0200

    index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,9 @@ export const navLinks = [
   { to: "/payments", label: "Payments" },
   { to: "/contact-us", label: "Contact Us" }
 ];
+
+export const statistics = [
+  { value: "8000+", label: "active users" },
+  { value: "250+", label: "Trusted companies" },
+  { value: "R500M+", label: "Transaction" }
+];
