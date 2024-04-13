1cb2570a8ad5befadffacbacc4affdf448f0667e
Seithati <151874357+Thati05@users.noreply.github.com>
2024-04-13 23:32:56 +0200

    Stats.jsx

--- src/sections/Stats.jsx
+++ src/sections/Stats.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { statistics } from '../constants';
 
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
-}
+    <section className='w-full justify-center items-center pl-[100px] max-container '>
 
-export default Stats
+      <div className='flex flex-1 flex-row items-center justify-center'>
+        <div className=' absolute  z-[0] w-[60%] h-[60%] -right-[40%]  rounded-full purple__gradient   ' />
+        {statistics.map((stat, index) => (
+          <div
+            className='pt-[100px] gap-3 flex flex-1 flex-row text-white items-center'
+            key={index}>
+            <p className=' font-Mont text-4xl font-bold' >{stat.value}</p>
+            <p className=' uppercase '>{stat.label}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
