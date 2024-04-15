9286d09ec582e69cec814095fdd3000b8dea2ff9
Seithati <151874357+Thati05@users.noreply.github.com>
2024-04-15 21:53:25 +0200

    Logos.jsx

--- src/sections/Logos.jsx
+++ src/sections/Logos.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { airbnb, binance, coinbase, dropbox } from "../assets"
 
 const Logos = () => {
   return (
-    <div>Logos</div>
+    <section className=' max-container padding-x w-full pt-[200px]'>
+      <div className=' flex flex-1 flex-row justify-center items-center gap-[120px]'>
+        <img src={airbnb} />
+        <img src={binance} />
+        <img
+          className="h-[35px]"
+          src={coinbase} />
+        <img src={dropbox} />
+
+      </div>
+    </section>
   )
 }
 
