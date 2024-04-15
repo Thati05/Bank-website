207701b1bcb63a886566f2a4452ac471c14113ba
Seithati <151874357+Thati05@users.noreply.github.com>
2024-04-15 21:52:41 +0200

    Testimonals.jsx

--- src/sections/Testimonals.jsx
+++ src/sections/Testimonals.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { testimonals } from "../constants"
+import UserReviews from "../components/UserReviews"
 
 const Testimonals = () => {
   return (
-    <div>Testimonals</div>
+    <section className=' max-container padding-x w-full justify-center items-center mt-[200px] '>
+
+      <div className=' absolute  z-[0] w-[60%] h-[10%] -right-[40%]  rounded-full orange__gradient' />
+
+      <div className=' flex felx-1 flex-row text-white gap-36 items-center justify-center  '>
+        <h2 className=' text-4xl  font-bold capitalize w-[464px] '>what are people saying about us?</h2>
+        <p className='font-light items-end w-[466px]'>Lorem ipsum dolor sit amet consectetur. Enim semper sed amet velit nunc id in.</p>
+      </div>
+      <div className=' grid-rows-3 flex flex-1 items-center grid-cols-1 mt-[100px] justify-around gap-[100px]'>
+        {testimonals.map((test) => (
+          <UserReviews key={test.name} {...test} />
+        ))}
+      </div>
+
+    </section>
   )
 }
 
